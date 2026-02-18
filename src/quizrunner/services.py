35bf50afaeaf2cf0fd5c3f8991e0558e9a2be7e3
Mr Vitalii <vitaliart7@gmail.com>
2026-02-18 18:51:49 +0300

    Day 2-3: Add validate varsion 1.1.0

--- src/quizrunner/services.py
+++ src/quizrunner/services.py
@@ -1,4 +1,6 @@
+# from .models import Choice, Question, TestSet
 from pathlib import Path
+# from typing import Any,Dict,Optional
 import json
 
 
@@ -14,7 +16,7 @@ def load_test_from_json(file_name):
 
   else:
     try:
-      with open(file_name,"r") as f:
+      with open(file_name,"r",encoding="utf-8") as f:
         data = json.load(f)
         return data
 
@@ -30,4 +32,45 @@ def load_test_from_json(file_name):
 
 
 data = load_test_from_json("tests_data/good_test.json")
-print(data)
+def validate_test(data):
+  errors = []
+  if not "title" in data:
+    errors.append("Ошибка нету блока title")
+
+  title = data["title"]
+
+  try:
+
+    title_probel = title.strip()
+
+    if len(title_probel) == 0:
+      errors.append("Ошибка в title не может быть пустой строкой или состоять из пробелов")
+
+  except AttributeError:
+    if title != str(title):
+      errors.append("Ошибка в title должнен быть строкой")
+
+
+  if len(title) < 1 or len(title) > 120:
+    errors.append("Ошибка в title не может быть больше 120 символов")
+
+  try:
+
+    description_strip = data["description"]
+
+    if description_strip.strip() == "":
+      errors.append("Проблема в description не может состоять из пробелов или быть пустым")
+
+  except KeyError:
+    if not "questions" in data:
+      errors.append("Ошибка нету блока questions")
+    if not "description" in data:
+      errors.append("Ошибка нету блока description ")
+
+  if len(data["questions"][0]["text"]) == 0:
+    errors.append("Вопрос не может быть пустым")
+
+
+  
+  print(errors)
+validate_test(data)
